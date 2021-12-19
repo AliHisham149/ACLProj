ebe7dd74802f11d3ede6ccfe7da7647e45d79394
AlyElashram <71949028+AlyElashram@users.noreply.github.com>
2021-12-19 22:07:37 +0100

    User Schema Created

---
+++ backend/Models/users.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    Uid: {
+        type: String,
+        required: true,
+    },
+    Name: {
+        type: String,
+        required: true,
+    },
+    Email: {
+        type: String,
+        required: true,
+    },
+    Password: {
+        type: String,
+        required: true,
+      },
+    DOB: {
+        type: Date,
+        required: true,
+      },
+},
+    { timestamps: false });
+
+const user = mongoose.model('users', userSchema);
+module.exports = user;
